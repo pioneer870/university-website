033d5c798bc8c27916b464b08b9ce7b610a89519
pioneer870 <pioneerone921@gmail.com>
2025-08-24 19:53:28 -0500

    Add files via upload

---
+++ university website/script.js
@@ -0,0 +1,10 @@
+// Toggle menu for navigation
+const navLinks = document.querySelector('.nav-links');
+
+function showMenu() {
+  navLinks.style.right = '0';
+}
+
+function hideMenu() {
+  navLinks.style.right = '-200px';
+}
